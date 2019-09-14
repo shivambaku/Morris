f9f8d04e4f34a3e5b247fce9e998a17bb5d5a66c
Shivam Kumar <hei@Shivams-MacBook-Pro.local>
2019-09-13 19:24:17 -0700

    removed unnecessary batch files

--- morris_ui/package.cmd
+++
@@ -1,4 +0,0 @@
-(npm -g list electron-packager || ^
-npm install -g electron-packager) && ^
-electron-packager . --asar --out="../ML UI Executable/" && ^
-xcopy "%~dp0cpp_addons\lib\*.dll" "%~dp0\..\ML UI Executable\ML-UI-win32-x64" /Y
